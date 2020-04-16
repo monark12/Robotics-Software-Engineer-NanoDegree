01e32ec2afcf4ac036aabe1e2bb8b171f6a672d4
Monark Unadkat <monarkunadkat12@gmail.com>
2020-04-16 03:47:40 +0000

    fixed

--- catkin_ws/build/cmake_install.cmake
+++ catkin_ws/build/cmake_install.cmake
@@ -131,8 +131,6 @@ if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   include("/home/workspace/catkin_ws/build/ball_chaser/cmake_install.cmake")
   include("/home/workspace/catkin_ws/build/pgm_map_creator/cmake_install.cmake")
   include("/home/workspace/catkin_ws/build/amcl/cmake_install.cmake")
-  include("/home/workspace/catkin_ws/build/costmap_2d/cmake_install.cmake")
-  include("/home/workspace/catkin_ws/build/move_base/cmake_install.cmake")
 
 endif()
 
