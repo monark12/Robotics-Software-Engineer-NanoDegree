c8b1116e07f7f408e0b9cec5ab38c1034cadbffa
Monark Unadkat <monark.unadkat@searce.com>
2020-04-25 21:07:21 +0530

    added first project

--- catkin_ws/build/ball_chaser/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/workspace/catkinws/src/ball_chaser
-# Build directory: /home/workspace/catkinws/build/ball_chaser
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- catkin_ws/devel/share/ball_chaser/cmake/ball_chaser-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(ball_chaser_MESSAGE_FILES "")
-set(ball_chaser_SERVICE_FILES "/home/workspace/catkinws/src/ball_chaser/srv/DriveToTarget.srv")
